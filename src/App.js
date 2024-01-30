835baa70c1ebf5c7fa62c099be13bc49b9bbf942
gurnemat <151676705+gurnemat@users.noreply.github.com>
2024-01-30 13:54:48 -0800

    added hero picture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
+import { IoHome } from "react-icons/io5";
+import HomeHero from "./components/HomeHero/HomeHero";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomeHero />
     </div>
   );
 }
---
+++ src/components/HomeHero/HomeHero.js
@@ -0,0 +1,17 @@
+import "./HomeHero.scss";
+import CakeImage from "../../assets/logo/Cake.webp";
+import { Link } from "react-router-dom";
+
+const HomeHero = () => {
+  return (
+    <div className="home-hero">
+      <img src={CakeImage} alt="" />
+      <div className="home-hero__content">
+        <h1>SugarSymphony</h1>
+        <a href="/">recipes</a>
+      </div>
+    </div>
+  );
+};
+
+export default HomeHero;
